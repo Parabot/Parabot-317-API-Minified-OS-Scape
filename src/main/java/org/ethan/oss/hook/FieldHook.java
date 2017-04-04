503b07c819eec6ee543dad28f1b8f46bba5b81d6
JKetelaar <jeroen@ketelaar.me>
2017-04-04 22:54:57 +0200

    [FEATURE] Added into and getters to FieldHook

--- src/main/java/org/ethan/oss/hook/FieldHook.java
+++ src/main/java/org/ethan/oss/hook/FieldHook.java
@@ -4,12 +4,28 @@ public class FieldHook {
     String clazz;
     String field;
     int    mult;
+    String into;
 
-    public FieldHook(String clazz, String field, int getterMult) {
+    public FieldHook(String clazz, String field, int getterMult, String into) {
         this.clazz = clazz;
         this.field = field;
         this.mult = getterMult;
-
+        this.into = into;
     }
 
+    public String getClazz() {
+        return clazz;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public int getMult() {
+        return mult;
+    }
+
+    public String getInto() {
+        return into;
+    }
 }
