518938211e6e8c28519b1080f6e69fe096258dfb
JKetelaar <jeroen@ketelaar.me>
2017-04-11 12:06:15 +0200

    [TASK] Updated Npc debug to draw Npc

--- src/main/java/org/parabot/osscape/debug/DNpcs.java
+++ src/main/java/org/parabot/osscape/debug/DNpcs.java
@@ -25,18 +25,9 @@ public class DNpcs extends AbstractDebugger {
 
     @Override
     public void paint(Graphics graphics) {
-        final FontMetrics metrics = graphics.getFontMetrics();
-
         for (Npc npc : Npcs.getNearest()) {
             if (npc != null && npc.isOnScreen()) {
-                Point point = npc.getPointOnScreen();
-
-                graphics.setColor(Color.BLUE);
-                graphics.fillRect(point.x, point.y, 5, 5);
-                graphics.setColor(Color.black);
-                String name = "[" + npc.getName() + " - " + npc.getDef().getId() + "] - " + npc.getLocation();
-                graphics.drawString(name, point.x - (metrics.stringWidth(name) / 2), point.y - 5);
-                npc.getLocation().draw((Graphics2D) graphics, Color.orange);
+                npc.draw((Graphics2D) graphics);
             }
         }
     }
